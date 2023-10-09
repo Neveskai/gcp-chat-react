be601b5b69d4e5bc869ecfa51fd27d84e2825e44
Wendell <wnevesky@gmail.com>
2023-10-09 12:26:40 -0300

    audio controls

--- src/components/AudioRecorder/index.tsx
+++ src/components/AudioRecorder/index.tsx
@@ -1,19 +1,45 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { AudioRecorder as AudioRecord, useAudioRecorder } from 'react-audio-voice-recorder'
+
 import VoiceIcon from '@mui/icons-material/KeyboardVoice'
-import StopIcon from '@mui/icons-material/Stop'
+import PauseIcon from '@mui/icons-material/Pause'
+import DeleteIcon from '@mui/icons-material/Delete'
+import SendIcon from '@mui/icons-material/Send'
 import Button from '@/components/Button'
 
 import './index.css'
+import React from 'react'
+
+const formatTime = (time: number) => {
+  let seconds = Math.floor(time)
+  let minutes = Math.floor(seconds / 60)
+  const hours = Math.floor(minutes / 60)
+
+  seconds -= minutes * 60
+  minutes -= hours * 60
+
+  return `${minutes}:${(seconds % 60).toString().padStart(2, '0')}`
+}
 
 function AudioRecorder() {
   const recorderControls = useAudioRecorder()
+  const [canceled, setCanceled] = React.useState(false)
 
   const onRecordComplete = (blob: Blob) => {
+    if (canceled) return setCanceled(false)
+
+    console.log(blob)
     return blob
   }
 
+  const onRecordCancel = () => {
+    setCanceled(true)
+
+    recorderControls.stopRecording()
+  }
+
   const isRecording = recorderControls.isRecording
+  const isPaused = recorderControls.isPaused
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -24,6 +50,7 @@ function AudioRecorder() {
           noiseSuppression: true,
           echoCancellation: true,
         }}
+        showVisualizer={true}
         downloadFileExtension='mp3'
       />
 
@@ -34,9 +61,32 @@ function AudioRecorder() {
       )}
 
       {isRecording && (
-        <Button variant='outlined' onClick={recorderControls.stopRecording}>
-          <StopIcon />
-        </Button>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '4px',
+            border: '1px solid #c6c6c6',
+            borderRadius: '4px',
+          }}
+        >
+          <Button onClick={onRecordCancel}>
+            <DeleteIcon />
+          </Button>
+
+          <Typography sx={{ marginTop: '2px', width: '64px', textAlign: 'center' }}>
+            {formatTime(recorderControls.recordingTime)}
+          </Typography>
+
+          <Button onClick={recorderControls.togglePauseResume} color='error'>
+            {!isPaused ? <PauseIcon /> : <VoiceIcon />}
+          </Button>
+
+          <Button onClick={recorderControls.stopRecording}>
+            <SendIcon />
+          </Button>
+        </Box>
       )}
     </Box>
   )
